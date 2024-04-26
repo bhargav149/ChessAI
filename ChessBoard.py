d22aec7417c9275edca358052c830fe494325cd7
user <adamjhall129@vt.edu>
2024-04-25 23:20:45 -0400

    switched to different method for board state

--- ChessBoard.py
+++ ChessBoard.py
@@ -1,334 +1,102 @@
-# pieces with W or B at the beginning of the string determines what team they belong to
-# N means none state
-import copy
+from functools import lru_cache
+import chess
+import chess.polyglot
 
-depthness = 2
+depthness = 10
 
+#Hash the board for fast access and cache
+chess.Board.__hash__ = chess.polyglot.zobrist_hash
+@lru_cache(maxsize=None)
+
+#Gets all legal moves and sort them for the best possible moves
+def getMoves(board):
+  moves = list(board.legal_moves)
+
+  #sort for best result
+  moves.sort(key=lambda move: board.is_capture(move), reverse=True)
+  return moves
+
+#Optimized minmax pruning trick for fast analysis
 def aphaBeta(boardState):
-  return maxF(0, boardState, -10000000, 10000000)
+  return maxF(0, boardState, float('inf'), float('-inf'))
 
-def maxF(depth, boardState, apha, beta):
-  if depth >= depthness:
-    return boardState.boardEval()
-  max = -100000000000
-  for state in getSucceedStates(boardState, "W"):
-    val = minF(depth, state, apha, beta)
+def maxF(depth, boardState, alpha, beta):
+  if depth >= depthness or boardState.is_game_over():
+    return (evalBoard(boardState), None)
 
-    if val > beta:
-      return val
+  max_value = float('-inf')
+  best_move = None
 
-    if apha < val:
-      apha = val
+  #Generate all moves for current turn
+  for move in getMoves(boardState):
 
-    if max < val:
-      max = val
+    #Change the board state to current move
+    boardState.push(move)
+    val, _ = minF(depth + 1, boardState, alpha, beta)
+    boardState.pop()
 
-  return max
+    if val >= beta:
+      return (val, move)
 
-def minF(depth, boardState, apha, beta):
-  if depth >= depthness:
-    return boardState.boardEval()
+    if val > max_value:
+      max_value = val
+      best_move = move
 
-  min = 100000000000
-  for state in getSucceedStates(boardState, "B"):
-    val = 0
-    val = maxF(depth + 1, state, apha, beta)
-    if val < apha:
-      return val
+    if alpha < val:
+      alpha = val
+
+  return (max_value, best_move)
+
+def minF(depth, boardState, alpha, beta):
+
+  #Stop at certain depth
+  if depth >= depthness or boardState.is_game_over():
+    return (evalBoard(boardState), None)
+
+  minVal = float('inf')
+
+  #Generate all moves for current turn
+  for move in getMoves(boardState):
+
+    #Change the board state to current move
+    boardState.push(move)
+    val, _ = maxF(depth + 1, boardState, alpha, beta)
+    boardState.pop()
+
+    if val <= alpha:
+      return (val, move)
+
+    if val < minVal:
+        minVal = val
 
     if beta > val:
       beta = val
 
-    if min > val:
-      min = val
+  return (minVal, None)
 
-  return min
+def evalBoard(board):
 
-def getSucceedStates(cB, team):
-  teamMove = None
-  if team == cB.white:
-    teamMove = cB.getWhitePieces()
-  else:
-    teamMove = cB.getBlackPieces()
-  boardStates = []
-  for fState in teamMove:
-    for tState in cB.legalMoves(cB.getPiece(fState), fState):
-      newBoard = ChessBoard(board_state = copy.deepcopy(cB.getBoard()))
-      newBoard.updateBoard(fState, tState)
-      boardStates.append(newBoard)
-  return boardStates
+  #Game end with checkmate
+  if board.is_checkmate():
+    #Who lost?
+    if board.turn:
+      return float('-inf')
+    else:
+        return float('inf')
 
-def replace(lis, elem1, elem2):
-  for i in range(0, len(lis)):
-    if elem1 == elem2:
-      lis[i] = elem2
-
-#Helps manage the states of the board
-#
-class ChessBoard():
-
-  def __init__(self, board_state = [["" for i in range(0, 8)] for j in range(0, 8)]):
-    self.board_state = board_state
-    self.white_pieces = []
-    self.black_pieces = []
-
-    self.white = "W"
-    self.black = "B"
-
-    self.queen = "QE"
-    self.queenVal = 10
-
-    self.pawn = "PA"
-    self.pawnVal = 1
-
-    self.knight = "KN"
-    self.knightVal = 4
-
-    self.king = "KI"
-    self.kingVal = 1000000
-
-    self.bishop = "BI"
-    self.bishopVal = 5
-
-    self.rook = "RO"
-    self.rookVal = 7
-
-    for i in range(0, 8):
-      for j in range(0, 8):
-        team = self.getTeam((i, j))
-        if team == self.white:
-          self.white_pieces.append([i, j])
-        elif team == self.black:
-          self.black_pieces.append([i, j])
-
-  def boardEval(self):
-    whiteScore = 0
-    blackScore = 0
-    for state in self.white_pieces:
-      whiteScore += self.scorePiece(state)
-
-    for state in self.black_pieces:
-      blackScore += self.scorePiece(state)
-
-    return whiteScore - blackScore
-
-  def scorePiece(self, state):
-    piece = self.getPiece(state)
-    if piece == self.queen:
-      return self.queenVal
-    elif piece == self.bishop:
-      return self.bishopVal
-    elif piece == self.rook:
-      return self.rookVal
-    elif piece == self.pawn:
-      return self.pawnVal
-    elif piece == self.knight:
-      return self.knightVal
-    elif piece == self.king:
-      return self.kingVal
+  #Stalemate
+  if board.is_stalemate():
     return 0
 
-  def getBoard(self):
-    return self.board_state
+  pieceVal = {chess.PAWN: 1, chess.KNIGHT: 4, chess.BISHOP: 5, chess.ROOK: 7, chess.QUEEN: 11}
+  #Check all material on the board
+  whiteVal = 0
+  blackVal = 0
+  for piece in board.piece_map().values():
+    if piece.color == chess.WHITE and piece.piece_type != chess.KING:
+      whiteVal += pieceVal[piece.piece_type]
+    if piece.color == chess.BLACK and piece.piece_type != chess.KING:
+      blackVal += pieceVal[piece.piece_type]
 
-  def getWhitePieces(self):
-    return self.white_pieces
-
-  def getBlackPieces(self):
-    return self.black_pieces
-
-  def legalMoves(self, piece, state):
-    moves = []
-    team = self.getTeam(state)
-
-    if piece == self.queen:
-      moves += self.straightMoves(state)
-      moves += self.diagnolMoves(state)
-    elif piece == self.bishop:
-      moves += self.diagnolMoves(state)
-    elif piece == self.rook:
-      moves += self.straightMoves(state)
-    elif piece == self.pawn:
-      moves += self.pawnMoves(state)
-    elif piece == self.knight:
-      moves += self.knightMoves(state)
-    elif piece == self.king:
-      moves += self.kingMoves(state)
-    return moves
-
-  def pawnMoves(self, state):
-    moves = []
-    currState = copy.deepcopy(state)
-
-    if self.getTeam(currState) == self.white:
-      dir = -1
-    else:
-      dir = 1
-
-    foward = [currState[0], currState[1] + dir]
-    if self.isEmpty(foward):
-        moves.append(foward)
-
-    leftCapture = [currState[0] - 1, currState[1] + dir]
-    if not self.isEmpty(leftCapture) and not self.isSameTeam(leftCapture, currState):
-        moves.append(leftCapture)
-
-    rightCapture = [currState[0] + 1, currState[1] + dir]
-    if not self.isEmpty(rightCapture) and not self.isSameTeam(rightCapture, currState):
-        moves.append(rightCapture)
-
-    return moves
-
-  def knightMoves(self, state):
-    moves = []
-    currState = copy.deepcopy(state)
-
-    potential = []
-    potential.append([currState[0] + 1, currState[1] + 2])
-    potential.append([currState[0] - 1, currState[1] + 2])
-    potential.append([currState[0] + 1, currState[1] - 2])
-    potential.append([currState[0] - 1, currState[1] - 2])
-
-    for p in potential:
-      if self.isLegal(potential, state):
-        moves.append(p)
-    return moves
-
-  def kingMoves(self, state):
-    moves = []
-    currState = copy.deepcopy(state)
-
-    potential = []
-    potential.append([currState[0] + 1, currState[1] + 1])
-    potential.append([currState[0] - 1, currState[1] + 1])
-    potential.append([currState[0] + 1, currState[1] - 1])
-    potential.append([currState[0] - 1, currState[1] - 1])
-    potential.append([currState[0], currState[1] - 1])
-    potential.append([currState[0], currState[1] + 1])
-    potential.append([currState[0] + 1, currState[1]])
-    potential.append([currState[0] - 1, currState[1]])
-
-    for p in potential:
-      if self.isLegal(potential, state):
-        moves.append(p)
-    return moves
-
-  def straightMoves(self, state):
-    moves = []
-    currState = [state[0] + 1, state[1]]
-    while self.isLegal(currState, state):
-      moves.append(currState)
-      if not self.isEmpty(currState):
-        break
-      currState = [currState[0] + 1, currState[1]]
-
-    currState = [state[0] - 1, state[1]]
-    while self.isLegal(currState, state):
-      moves.append(currState)
-      if not self.isEmpty(currState):
-        break
-      currState = [currState[0] - 1, currState[1]]
-
-    currState = [state[0], state[1] + 1]
-    while self.isLegal(currState, state):
-      moves.append(currState)
-      if not self.isEmpty(currState):
-        break
-      currState = [currState[0], currState[1] + 1]
-
-    currState = [state[0], state[1] - 1]
-    while self.isLegal(currState, state):
-      moves.append(currState)
-      if not self.isEmpty(currState):
-        break
-      currState = [currState[0], currState[1] - 1]
-    return moves
-
-  def diagnolMoves(self, state):
-    moves = []
-    currState = [state[0] + 1, state[1] + 1]
-    while self.isLegal(currState, state):
-      moves.append(currState)
-      if not self.isEmpty(currState):
-        break
-      currState = [state[0] + 1, state[1] + 1]
-
-    currState = [state[0] + 1, state[1] - 1]
-    while self.isLegal(currState, state):
-      moves.append(currState)
-      if not self.isEmpty(currState):
-        break
-      currState = [state[0] + 1, state[1] - 1]
-
-    currState = [state[0] - 1, state[1] + 1]
-    while self.isLegal(currState, state):
-      moves.append(currState)
-      if not self.isEmpty(currState):
-        break
-      currState = [state[0] - 1, state[1] + 1]
-
-    currState = [state[0] - 1, state[1] - 1]
-    while self.isLegal(currState, state):
-      moves.append(currState)
-      if not self.isEmpty(currState):
-        break
-      currState = [state[0] - 1, state[1] - 1]
-    return moves
-
-  def inBounds(self, state):
-    return 0 <= state[0] and state[0] < 8 and 0 <= state[1] and state[1] < 8
-
-  def updateBoard(self, fState, tState):
-    i, j = fState
-    i2, j2 = tState
-    team = self.getTeam(fState)
-
-    #Promotion piece detected
-    if self.getPiece(fState) == self.pawn and (j2 == 7 or j2 == 0):
-      self.board_state[i][j] = team + self.queen
-
-    #Capture piece
-    if not self.isEmpty(tState):
-      if team == self.white:
-        self.black_pieces.remove(tState)
-      else:
-        self.white_pieces.remove(tState)
-
-    #Move piece
-    if team == self.white:
-      replace(self.white_pieces, fState, tState)
-    else:
-      replace(self.black_pieces, fState, tState)
-
-    self.board_state[i2][j2] = self.board_state[i][j]
-    self.board_state[i][j] = ""
-
-  def getTeam(self, state):
-    i, j = state
-    return self.board_state[i][j][:1]
-
-  def getPiece(self, state):
-    i, j = state
-    return self.board_state[i][j][1:]
-
-  def isSameTeam(self, currentState, otherState):
-    if self.isEmpty(currentState) or self.isEmpty(otherState):
-      return False
-
-    return self.getTeam(currentState) == self.getTeam(otherState)
-
-  def isEmpty(self, state):
-    i, j = state
-    return self.board_state[i][j] == ""
-
-  def isLegal(self, currState, otherState):
-    if not self.inBounds(currState):
-      return False
-
-    return not self.isSameTeam(currState, otherState)
-
-  def __str__(self):
-    boardStr = ""
-    for row in self.board_state:
-      boardStr += str(row) + "\n"
-    return boardStr
+  score = whiteVal - blackVal
+  return score
